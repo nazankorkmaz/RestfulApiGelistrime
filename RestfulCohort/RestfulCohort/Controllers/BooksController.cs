7831361afb291975a9a3145f95a96610c3601c53
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-03-14 16:41:59 +0300

    duzenlendi.

--- RestfulCohort/RestfulCohort/Controllers/BooksController.cs
+++ RestfulCohort/RestfulCohort/Controllers/BooksController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RestfulCohort.Models;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace RestfulCohort.Controllers
 {
     [Route("api/[controller]")]
@@ -128,41 +126,5 @@ namespace RestfulCohort.Controllers
         return Ok(result.ToList());
     }
 
-
-
-        /*
-        // GET: api/<BooksController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<BooksController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<BooksController>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<BooksController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<BooksController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-
-        */
     }
 }
